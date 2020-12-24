7ef207b377e9e856980dc30ff6bdfd4496a68d66
lysak.ilya <ilsy@brighta.biz>
2020-12-24 19:00:38 +0200

    header+adaptive

---
+++ src/js/func.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+
+import $ from 'jquery';
+
+$('.icon-menu').click(function(event) {
+    $(this).toggleClass('active');
+    $('.menu__body').toggleClass('active');
+    $('body').toggleClass('lock')
+});
+
+function ibg() {
+
+    $.each($('.ibg'), function (index, val) {
+        if ($(this).find('img').length > 0) {
+            $(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
+        }
+    });
+}
+
+ibg();
